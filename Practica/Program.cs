940c1d989262594dfb1162ea55c2cf2e0c56447d
Kast1lo <152639426+Kast1lo@users.noreply.github.com>
2025-09-16 20:53:34 +0500

    Добавьте файлы проекта.

---
+++ Practica/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число: ");
+string bin = Console.ReadLine();
+int dec = Convert.ToInt32(bin, 2);
+Console.WriteLine($"число:{dec}");
+
+
+Console.Write("Введите второе число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+string bolt = Convert.ToString(value, 2);
+Console.WriteLine($"число: {bolt}");
+
+
+int N = int.Parse(Console.ReadLine());
+int M = int.Parse(Console.ReadLine());
+int T = int.Parse(Console.ReadLine());
+int totalMinutes = N * 60 + M;
+totalMinutes += T;
+totalMinutes %= 1440;
+int hours = totalMinutes / 60;
+int minutes = totalMinutes % 60;
+Console.WriteLine($"{hours:D2}:{minutes:D2}");
